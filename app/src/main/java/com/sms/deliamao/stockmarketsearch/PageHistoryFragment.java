a9308dfd1fa01b30e7823515f253c9c1785d6914
Delia Mao <deliamao@Delias-MacBook-Pro.local>
2016-05-04 20:58:56 -0700

    commit version

--- app/src/main/java/com/sms/deliamao/stockmarketsearch/PageHistoryFragment.java
+++ app/src/main/java/com/sms/deliamao/stockmarketsearch/PageHistoryFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 /**
  * Created by deliamao on 5/2/16.
@@ -19,7 +21,12 @@ public class PageHistoryFragment extends Fragment {
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_historical, container, false);
+        View view = inflater.inflate(R.layout.fragment_historical, container, false);
+        WebView wv = (WebView) view.findViewById(R.id.webView);
+        WebSettings webSettings = wv.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        wv.loadDataWithBaseURL("file:///android_asset/historical.html", content, "text/html", "UTF-8", null);
+        return view;
     }
 }
 
--- app/src/main/java/com/sms/deliamao/stockmarketsearch/TestHistorical.java
+++ app/src/main/java/com/sms/deliamao/stockmarketsearch/TestHistorical.java
@@ -12,11 +12,9 @@ public class TestHistorical extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_historical);
-<<<<<<< HEAD
-        WebView webViewer = (WebView) findViewById(R.id.historical_chart_webView);
-=======
+
         WebView webViewer = (WebView) findViewById(R.id.webView);
->>>>>>> 5977602b1ef32dc97e834d0d7d03cd6ed85b612d
+
         webViewer.loadUrl("fragment_historical.html");
 
     }
